2a3fc42ccced2a4d01604ab95a4e035962992926
Ild0O <garciaemmanuel@hotmail.com>
2022-06-05 10:24:06 +0200

    responsive nav accueil

--- composant/accueil.php
+++ composant/accueil.php
@@ -1,38 +1,38 @@
 
-<!-- <section class="main__background"></section>
+<img class="section__bannier-img" src="public/img/banniereP.jpg" alt="">
 
-<section class="main__navbar">
-    <ul class="navbar__icon">
-        <li class="li__icon li__green">
-            <a href="">
+<nav class="main__navbar-icon">
+    <ul class="navbar__ul-icon">
+        <li class="ul__li-icon li__green">
+            <a class="li__a-icon" href="">
                 <img src="public/icon/icons8-camion-vérifié-64.png" alt="">
                 <h3>Livraison à domicile</h3>
                 <p>Livraison en 4H sur Ville ou en 24H avec Retardpost</p>
             </a>                 
         </li>
 
-        <li class="li__icon li__orange">
-            <a href="">
+        <li class="ul__li-icon li__orange">
+            <a class="li__a-icon" href="">
                 <img src="public/icon/icons8-petite-entreprise-64.png" alt="">
                 <h3>Livraison à domicile</h3>
                 <p>Livraison en 4H sur Ville ou en 24H avec Retardpost</p>
             </a>                 
         </li>
 
-        <li class="li__icon li__green">
-            <a href="">
+        <li class="ul__li-icon li__green">
+            <a class="li__a-icon" href="">
                 <img src="public/icon/icons8-service-64.png" alt="">
                 <h3>Livraison à domicile</h3>
                 <p>Livraison en 4H sur Ville ou en 24H avec Retardpost</p>
             </a>                 
         </li>
 
-        <li class="li__icon li__orange">
-            <a href="">
+        <li class="ul__li-icon li__orange">
+            <a class="li__a-icon" href="">
                 <img src="public/icon/icons8-garantie-64.png" alt="">
                 <h3>Livraison à domicile</h3>
                 <p>Livraison en 4H sur Ville ou en 24H avec Retardpost</p>
             </a>                 
         </li>   
     </ul>            
-</section> -->
+</nav> 
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'composant/head.php' ?>
-    <main>
-        <?php include 'composant/accueil.php' ?>
-    </main>
+<main>
+    <?php include 'composant/accueil.php' ?>
+</main>
 <?php include 'composant/footer.php' ?>
